a4693213cd2b6d75869ff6ea46a3b187a7b90c50
albertdolata <albdolcode@gmail.com>
2023-04-07 13:08:52 +0200

    Show account balance in console

--- dart_variables.dart
+++ dart_variables.dart
@@ -2,4 +2,12 @@ void main() {
   int age; //define integer variable
   age = 22; //initialising integer variable
   print(age); //print age value in console
+  double balance; //define double variable
+  balance = 300.78; //initialising double variable
+  /*
+    define and initialising String variable,
+    use '$' to interpolate int variable to string
+  */
+  String balance_string = "Account balance: $balance PLN";
+  print(balance_string); //print account balance in console
 }
