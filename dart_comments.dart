2cb7738cd8c8b6bf11f59a89d57ce6fda6905ce5
albertdolata <albdolcode@gmail.com>
2023-04-07 11:58:33 +0200

    Add dart_comment file

---
+++ dart_comments.dart
@@ -0,0 +1,14 @@
+// single line comment
+/*  multiple
+line
+comment
+ */
+/*
+shortcut for a multiple line comment ctrl + shit +/ */
+// shortcut for a single line comment ctrl + /
+/// single line doc comment
+/** multiple
+ * line
+ * doc
+ * comment
+ */
