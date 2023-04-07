9329ec36eaddbfe28148015e4fe8a8cf97d9de8e
albertdolata <albdolcode@gmail.com>
2023-04-07 13:13:53 +0200

    Fix int to double in comment

--- dart_variables.dart
+++ dart_variables.dart
@@ -6,7 +6,7 @@ void main() {
   balance = 300.78; //initialising double variable
   /*
     define and initialising String variable,
-    use '$' to interpolate int variable to string
+    use '$' to interpolate double variable to string
   */
   String balance_string = "Account balance: $balance PLN";
   print(balance_string); //print account balance in console
