7727ada5b3065df3558896e3d675656781e9ba87
albertdolata <albdolcode@gmail.com>
2023-04-10 12:39:52 +0200

    Add 'age' input

--- dart_standartInputOutput.dart
+++ dart_standartInputOutput.dart
@@ -3,5 +3,8 @@ import 'dart:io';
 
 void main(){
       //asking user for a name
-    print('Hello what is your name?');
+    print('Hello what is your age?');
+
+    //taking input from user
+    int? age = int.parse(stdin.readLineSync()!);
 }
