93701d2d2737bb987e5197fd407aea59793d15b1
albertdolata <albdolcode@gmail.com>
2023-04-10 15:39:30 +0200

    Add use of test type operators on variables

--- dart_operators.dart
+++ dart_operators.dart
@@ -8,6 +8,7 @@ void main(){
     print('Enter number b');
     int? b = int.parse(stdin.readLineSync()!);
 
+
     //use of arithmetical operators
     print('Sum of a and b is ${a + b}');
     print('The difference between a and b is ${a - b}');
@@ -16,4 +17,17 @@ void main(){
     print('The quotient of a and b is ${a / b}');
     print('The quotient of a and b is ${a ~/ b} (result as integer)');
     print('The reminder of a and b is ${a % b}');
+
+    //use of relational operators
+    print('a is greater than b is ${a>b}');
+    print('a is smaller than b is ${a<b}');
+    print('a is greater or equal to b is ${a>=b}');
+    print('a is smaller or equal to b is ${a<=b}');
+    print('a is equal to b is ${a==b}');
+    print('a is not equal to b is ${a!=b}');
+
+    //use of test type operators
+    print('Is a a double? Answer: ${a is double}  ');
+    print('Is a a String? Answer: ${a is String}  ');
+    print('a is not an String? Answer: ${a is! String}  ');
 }
