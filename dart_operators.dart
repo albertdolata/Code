bd1256dda514d14620c41e687f33d7fefc16a635
albertdolata <albdolcode@gmail.com>
2023-04-10 13:29:47 +0200

    Change inputs name, add arithmetical operations on variables a and b

--- dart_operators.dart
+++ dart_operators.dart
@@ -1,10 +1,19 @@
 import 'dart:io';
 
 void main(){
-    ////taking input from user
-    print('Enter yours mom age');
-    int? motherAge = int.parse(stdin.readLineSync()!);
+    //taking input from user
+    print('Enter number a');
+    int? a = int.parse(stdin.readLineSync()!);
 
-    print('Enter your age');
-    int? userAge = int.parse(stdin.readLineSync()!);
+    print('Enter number b');
+    int? b = int.parse(stdin.readLineSync()!);
+
+    //use of arithmetical operators
+    print('Sum of a and b is ${a + b}');
+    print('The difference between a and b is ${a - b}');
+    print('The negation of a is ${-a} and b is ${-b}');
+    print('The product of a and b is ${a * b}');
+    print('The quotient of a and b is ${a / b}');
+    print('The quotient of a and b is ${a ~/ b} (result as integer)');
+    print('The reminder of a and b is ${a % b}');
 }
