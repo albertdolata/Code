fd7302fdf582b3bb869c9ccf01479b88b48b9843
albertdolata <albdolcode@gmail.com>
2023-04-10 12:43:21 +0200

    Add output by using print() function

--- dart_standartInputOutput.dart
+++ dart_standartInputOutput.dart
@@ -3,8 +3,11 @@ import 'dart:io';
 
 void main(){
       //asking user for a name
-    print('Hello what is your age?');
+    print('Hello, what is your age?');
 
     //taking input from user
     int? age = int.parse(stdin.readLineSync()!);
+
+    //creating output by using print() function
+    print('Now I know that your age is $age');
 }
