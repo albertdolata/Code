1346bcbbd4a82acf1f0b7dca1e430dbc869dac3a
albertdolata <albdolcode@gmail.com>
2023-04-10 13:09:49 +0200

    Add library and data inputs

--- dart_operators.dart
+++ dart_operators.dart
@@ -1,3 +1,10 @@
-void main(){
+import 'dart:io';
 
+void main(){
+    ////taking input from user
+    print('Enter yours mom age');
+    int? motherAge = int.parse(stdin.readLineSync()!);
+
+    print('Enter your age');
+    int? userAge = int.parse(stdin.readLineSync()!);
 }
