e1dc7faea7da986a3b53d90827ed9b04c0fba108
albertdolata <albdolcode@gmail.com>
2023-04-11 18:19:17 +0200

    Add variables

--- task_numbers.dart
+++ task_numbers.dart
@@ -1,5 +1,8 @@
 import 'dart:io';
 
 void main(){
-
+  print('Enter numbers');
+  String stringOfNumbers = stdin.readLineSync() ?? '';
+  String evenNumbers, oddNumbers, sumOfEvenNumbers, sumOfOddNumbers;
+  int length = stringOfNumbers.length;
 }
