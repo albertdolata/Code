0fd468f3fc620d1f352cad6fe4a3c6e73728b859
albertdolata <albdolcode@gmail.com>
2023-04-11 19:49:14 +0200

    Add new variable,add loop to create a reversed word,add print output data

--- task_reverse_words.dart
+++ task_reverse_words.dart
@@ -3,7 +3,10 @@ import 'dart:io';
 void main(){
   print('Enter sentence');
   String sentence = stdin.readLineSync() ?? '';
-  String reversedSentence;
-
-
+  String reversedSentence = '';
+  int length = sentence.length;
+  for(int i = length-1; i >= 0; i-- ){
+    reversedSentence += sentence[i];
+  }
+print(reversedSentence);
 }
