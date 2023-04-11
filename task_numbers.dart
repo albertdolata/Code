a5f7cdfc7d55e2f1be826734c52aa01e85dfd334
albertdolata <albdolcode@gmail.com>
2023-04-11 21:11:53 +0200

    Fix print addition of numbers

--- task_numbers.dart
+++ task_numbers.dart
@@ -3,21 +3,23 @@ import 'dart:io';
 void main(){
   print('Enter numbers');
   String stringOfNumbers = stdin.readLineSync() ?? '';
-  String evenNumbers ='', oddNumbers = '';
+  String evenNumbers ='', oddNumbers = '', additionOfEvenNumbers = '', additionOfOddNumbers = '';
   int sumOfEvenNumbers = 0, sumOfOddNumbers = 0;
   int length = stringOfNumbers.length;
   for(int i=0; i < length; i++){
     int tempMemory = int.parse(stringOfNumbers[i]);
     if( tempMemory % 2 == 0 ){
-    evenNumbers += '$tempMemory ';
-    sumOfEvenNumbers += tempMemory;
+      evenNumbers += '$tempMemory ';
+      additionOfEvenNumbers +='+$tempMemory ';
+      sumOfEvenNumbers += tempMemory;
     }else{
-    oddNumbers += '$tempMemory ';
-    sumOfOddNumbers += tempMemory;
-  }
+      oddNumbers += '$tempMemory ';
+      additionOfOddNumbers +='+$tempMemory';
+      sumOfOddNumbers += tempMemory;
+    }
   }
   print('Even numbers:$evenNumbers');
-  print('Sum of even number:$sumOfEvenNumbers');
+  print('Sum of even number: ${additionOfEvenNumbers.substring(1)}=$sumOfEvenNumbers');
   print('Odd numbers:$oddNumbers');
-  print('Sum of odd numbers:$sumOfOddNumbers');
+  print('Sum of odd numbers: ${additionOfOddNumbers.substring(1)}=$sumOfOddNumbers');
 }
