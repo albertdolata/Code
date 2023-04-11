32b6b9745d1f358185790bc7fa7cdbe05203e853
albertdolata <albdolcode@gmail.com>
2023-04-11 18:55:03 +0200

    Changes in variables, add to if statement addition of odd and even numbers, add print statement for outputs data

--- task_numbers.dart
+++ task_numbers.dart
@@ -3,16 +3,21 @@ import 'dart:io';
 void main(){
   print('Enter numbers');
   String stringOfNumbers = stdin.readLineSync() ?? '';
-  String evenNumbers ='', oddNumbers = '', sumOfEvenNumbers = '', sumOfOddNumbers = '';
+  String evenNumbers ='', oddNumbers = '';
+  int sumOfEvenNumbers = 0, sumOfOddNumbers = 0;
   int length = stringOfNumbers.length;
   for(int i=0; i < length; i++){
     int tempMemory = int.parse(stringOfNumbers[i]);
     if( tempMemory % 2 == 0 ){
-    evenNumbers += ' $tempMemory';
+    evenNumbers += '$tempMemory ';
+    sumOfEvenNumbers += tempMemory;
     }else{
-    oddNumbers += ' $tempMemory';
+    oddNumbers += '$tempMemory ';
+    sumOfOddNumbers += tempMemory;
   }
   }
-  print('Even numbers: $evenNumbers');
-  print('Odd numbers: $oddNumbers');
+  print('Even numbers:$evenNumbers');
+  print('Sum of even number:$sumOfEvenNumbers');
+  print('Odd numbers:$oddNumbers');
+  print('Sum of odd numbers:$sumOfOddNumbers');
 }
