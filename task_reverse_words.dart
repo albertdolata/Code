839ffa58935269b82065d14b51f85633b7a3c83c
albertdolata <albdolcode@gmail.com>
2023-04-12 19:32:12 +0200

    Create function getNumbers

--- task_reverse_words.dart
+++ task_reverse_words.dart
@@ -4,9 +4,10 @@ void main() {
   print('Enter sentence');
   String sentence = stdin.readLineSync() ?? '';
   String reversedSentence = '';
-  int length = sentence.length;
-  for (int i = length - 1; i >= 0; i--) {
+
+  for (int i = sentence.length - 1; i >= 0; i--) {
     reversedSentence += sentence[i];
   }
+
   print(reversedSentence);
 }
