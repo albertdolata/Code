5694ab35064efcb3d2ba36e7c31d91b37e502e2d
albertdolata <albdolcode@gmail.com>
2023-04-12 19:46:06 +0200

    Create function getSentence, reverseSentence and used it in code

--- task_reverse_words.dart
+++ task_reverse_words.dart
@@ -1,13 +1,21 @@
 import 'dart:io';
 
 void main() {
-  print('Enter sentence');
-  String sentence = stdin.readLineSync() ?? '';
-  String reversedSentence = '';
+  String sentence = getSentence();
+  String reversedSentence = reverseSentence(sentence);
+  print(reversedSentence);
+}
 
+String getSentence(){
+  print('Enter sentence');
+  String userSentence = stdin.readLineSync() ?? '';
+  return userSentence;
+}
+
+String reverseSentence(String sentence){
+  String reversedSentence ='';
   for (int i = sentence.length - 1; i >= 0; i--) {
     reversedSentence += sentence[i];
   }
-
-  print(reversedSentence);
+  return reversedSentence;
 }
