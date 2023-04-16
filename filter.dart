559bd9d7406f5856a375ffb17e38402fd07dc3bd
albertdolata <albdolcode@gmail.com>
2023-04-16 13:23:55 +0200

    Change name of class to SumFilter, add method to class SumFilter that prints sum of even and odd numbers,

--- filter.dart
+++ filter.dart
@@ -1,18 +1,32 @@
-class Filter {
+class SumFilter {
   String numbers = '';
 
-  Filter(this.numbers);
+  SumFilter(this.numbers);
 
   String filterNumbers(){
     String evenNumbers = '';
     String oddNumbers = '';
     for(int i = 0; i < numbers.length; i++){
-      if(int.parse(numbers[i]) % 2 ==0){
+      if(int.parse(numbers[i]) % 2 == 0){
         evenNumbers += '${numbers[i]},';
       }else{
         oddNumbers += '${numbers[i]},';
       }
     }
     return 'Even numbers:$evenNumbers \nOdd numbers:$oddNumbers';
+  }
+
+  String addUpNumbers(){
+    int sumOfEvenNumbers = 0;
+    int sumOfOddNumbers = 0;
+    for(int i =0; i < numbers.length; i++){
+      if(int.parse(numbers[i]) % 2 == 0){
+        sumOfEvenNumbers += int.parse(numbers[i]);
+      }else{
+        sumOfOddNumbers += int.parse(numbers[i]);
+      }
+    }
+    return 'Sum of even numbers: $sumOfOddNumbers '
+        '\nSum of odd numbers: $sumOfOddNumbers';
   }
 }
