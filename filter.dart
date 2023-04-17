d55c887792abce3d986ca312f99b70957f06242d
albertdolata <albdolcode@gmail.com>
2023-04-17 11:31:42 +0200

    Add new variables to function addUpNumbers and use them for printing mathematical operation

--- filter.dart
+++ filter.dart
@@ -19,14 +19,20 @@ class SumFilter {
   String addUpNumbers(){
     int sumOfEvenNumbers = 0;
     int sumOfOddNumbers = 0;
+    String operationSumOfEvenNumbers = '';
+    String operationSumOfOddNumbers = '';
     for(int i =0; i < numbers.length; i++){
       if(int.parse(numbers[i]) % 2 == 0){
         sumOfEvenNumbers += int.parse(numbers[i]);
+        operationSumOfEvenNumbers += '+ ${numbers[i]} ';
       }else{
         sumOfOddNumbers += int.parse(numbers[i]);
+        operationSumOfOddNumbers += '+ ${numbers[i]} ';
       }
     }
-    return 'Sum of even numbers: $sumOfEvenNumbers '
-        '\nSum of odd numbers: $sumOfOddNumbers';
+    return 'Sum of even numbers: ${operationSumOfEvenNumbers.substring(1)} '
+        '= $sumOfEvenNumbers '
+        '\nSum of odd numbers: ${operationSumOfOddNumbers.substring(1)} '
+        '= $sumOfOddNumbers';
   }
 }
