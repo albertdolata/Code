36cae66c9855c10ffb5e59289ffdbe717dc32116
albertdolata <albdolcode@gmail.com>
2023-04-18 11:49:48 +0200

    Create filter functions

--- sum_filter.dart
+++ sum_filter.dart
@@ -15,6 +15,33 @@ class SumFilter {
     }
     return 'Even numbers : $evenNumbers \nOdd numbers:$oddNumbers';
   }
+String filterEvenNumbers() {
+  String evenNumbers = '';
+  for (int i = 0; i < _numbers.length; i++) {
+    if (int.parse(_numbers[i]) % 2 == 0) {
+      evenNumbers += '${_numbers[i]},';
+    }
+  }
+  return evenNumbers;
+}
+
+String filterOddNumbers(){
+    String oddNumbers = '';
+    for (int i = 0; i < _numbers.length; i++) {
+      if (int.parse(_numbers[i]) % 2 == 1) {
+        oddNumbers += '${_numbers[i]},';
+      }
+    }
+    return oddNumbers;
+}
+
+  void showEvenNumbers(){
+    print('Even numbers : $filterEvenNumbers');
+  }
+
+  void showOddNumbers(){
+    print('Even numbers : $filterOddNumbers');
+  }
 
   String addUpNumbers(){
     int sumOfEvenNumbers = 0;
@@ -35,4 +62,13 @@ class SumFilter {
         '\nSum of odd numbers:${operationSumOfOddNumbers.substring(1)} '
         '= $sumOfOddNumbers';
   }
+}
+void showSumOfEvenNumbers(){
+  print('Sum of even numbers:${operationSumOfEvenNumbers.substring(1)} '
+      '= $sumOfEvenNumbers ');
+}
+
+void showOfOddNumbers(){
+  print('Sum of odd numbers:${operationSumOfOddNumbers.substring(1)} '
+  '= $sumOfOddNumbers');
 }
