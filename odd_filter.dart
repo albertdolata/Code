c775ed57bf2d7920ea973699807af8863b8b7e1b
albertdolata <albdolcode@gmail.com>
2023-05-05 14:09:37 +0200

    Override method filterNumbers in class OddFilter

--- odd_filter.dart
+++ odd_filter.dart
@@ -2,4 +2,15 @@ import 'filter.dart';
 
 class OddFilter extends Filter{
   OddFilter(String numbers) : super (numbers);
+
+  @override
+  List filterNumbers(List numbers){
+    List oddNumbers = [];
+    for (int i = 0; i < numbers.length; i++) {
+      if (!isEven(numbers, i)) {
+        oddNumbers.add(numbers[i]);
+      }
+    }
+    return oddNumbers;
+  }
 }
