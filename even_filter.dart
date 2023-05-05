388354d01f02976834040187ffa15a87cd59da51
albertdolata <albdolcode@gmail.com>
2023-05-05 13:43:17 +0200

    Override method filterNumbers in class EvenFilter

--- even_filter.dart
+++ even_filter.dart
@@ -2,4 +2,15 @@ import 'filter.dart';
 
 class EvenFilter extends Filter {
   EvenFilter(String numbers) : super(numbers);
+
+  @override
+  List filterNumbers(List numbers){
+    List evenNumbers = [];
+    for (int i = 0; i < numbers.length; i++) {
+      if (isEven(numbers, i)) {
+        evenNumbers.add(numbers[i]);
+      }
+    }
+    return evenNumbers;
+  };
 }
