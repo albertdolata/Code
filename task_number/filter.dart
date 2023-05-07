eb97e33ef13862e58966d7f3da87747127bc85d7
albertdolata <albdolcode@gmail.com>
2023-05-07 11:50:30 +0200

    Change implementation of convertStringToList method in class Filter

--- task_number/filter.dart
+++ task_number/filter.dart
@@ -3,14 +3,8 @@ abstract class Filter {
 
   Filter(this._numbers);
 
-  List convertStringToList(){
-    List numbers = [];
-    for (int i = 0; i < _numbers.length; i++) {
-      numbers.add(int.parse(_numbers[i]));
-    }
-    return numbers;
-  }
-
+  List convertStringToList() => _numbers.split("");
+  
   bool isEven(List numbers, int i) => numbers[i] % 2 == 0;
 
   List filterNumbers(List numbers);
