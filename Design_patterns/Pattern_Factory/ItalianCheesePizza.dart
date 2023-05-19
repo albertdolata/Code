0ebf13cd749fc2e977499e93e05481b1d4fad08c
Albert Dolata <albdolcode@gmail.com>
2023-05-19 22:39:25 +0200

    Create ItalianCheesePizza class

---
+++ Design_patterns/Pattern_Factory/ItalianCheesePizza.dart
@@ -0,0 +1,11 @@
+import 'Pizza.dart';
+
+class ItalianCheesePizza extends Pizza {
+  ItalianCheesePizza() {
+    name = "Italian cheese pizza with Marinara sauce";
+    dough = "Thing crusty dough";
+    sauce = "Marinara sauce";
+
+    toppings.add("Grated Reggiano cheese");
+  }
+}
