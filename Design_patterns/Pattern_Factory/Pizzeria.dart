46f9ee36788fac9319ba0ff2b94d0269070e4a4a
Albert Dolata <albdolcode@gmail.com>
2023-05-19 21:39:25 +0200

    Create class Pizzeria

---
+++ Design_patterns/Pattern_Factory/Pizzeria.dart
@@ -0,0 +1,19 @@
+import 'Pizza.dart';
+
+abstract class Pizzeria {
+
+  Pizza orderPizza(String type) {
+    Pizza pizza;
+
+    pizza = createPizza(type);
+
+    pizza.prepare();
+    pizza.bake();
+    pizza.cut();
+    pizza.pack();
+
+    return pizza;
+  }
+
+  Pizza createPizza(String type);
+}
