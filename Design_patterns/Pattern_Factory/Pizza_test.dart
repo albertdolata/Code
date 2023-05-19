51ab7e4ce633b536f3b3e05b2cfc043299e5dff6
Albert Dolata <albdolcode@gmail.com>
2023-05-19 22:52:21 +0200

    Create main function in Pizza_test file

---
+++ Design_patterns/Pattern_Factory/Pizza_test.dart
@@ -0,0 +1,15 @@
+import 'American_Pizzeria.dart';
+import 'Italian_Pizzeria.dart';
+import 'Pizza.dart';
+import 'Pizzeria.dart';
+
+void main() {
+  Pizzeria italian = ItalianPizzeria();
+  Pizzeria american = AmericanPizzeria();
+
+  Pizza pizza = italian.orderPizza("cheese");
+  print("Eryk ordered: " + pizza.getName());
+
+  pizza = american.orderPizza("cheese");
+  print("Jacek ordered: " + pizza.getName());
+}
