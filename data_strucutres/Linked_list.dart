ee76949d0cfdbc6013784e960bf546a68801c2b2
Albert Dolata <albdolcode@gmail.com>
2023-05-23 16:48:19 +0200

    Create insertData method in LinkedList class

--- data_strucutres/Linked_list.dart
+++ data_strucutres/Linked_list.dart
@@ -4,4 +4,19 @@ class LinkedList {
   dynamic head = null;
   dynamic tail;
   dynamic pointer;
+  Node node = Node();
+
+  void insertData(dynamic data) {
+    if(head == null){
+      head = data;
+      pointer = data;
+      node.data = data;
+      tail = data;
+    }else {
+      pointer = data;
+      node.next = pointer;
+      node.data = data;
+      tail = data;
+    }
+  }
 }
