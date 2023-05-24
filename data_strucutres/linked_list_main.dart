ef4fa32bc5d744b00589f6ea46209cb543d4a082
Albert Dolata <albdolcode@gmail.com>
2023-05-24 12:58:44 +0200

    Create function main to test Linked List

---
+++ data_strucutres/linked_list_main.dart
@@ -0,0 +1,13 @@
+import 'Linked_list.dart';
+
+void main() {
+  LinkedList linkedList = LinkedList();
+
+  linkedList.insertNewData(8);
+  linkedList.insertNewData(1);
+  linkedList.insertNewData(43);
+  linkedList.insertNewData(45);
+  linkedList.insertNewData(23);
+
+  linkedList.printLinkedList();
+}
