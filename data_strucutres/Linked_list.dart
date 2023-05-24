5f61d46f171567529434b5e3d00895647333ef55
Albert Dolata <albdolcode@gmail.com>
2023-05-24 12:50:53 +0200

    Change implementation of method initializeHead

--- data_strucutres/Linked_list.dart
+++ data_strucutres/Linked_list.dart
@@ -3,9 +3,8 @@ import 'Node.dart';
 class LinkedList {
   Node? head;
 
-  void initializeLinkedList(dynamic data){
-    Node headNode = Node(data);
-    return headNode.data;
+  void initializeHead(dynamic data) {
+    head = Node(data);
   }
 
   void insertData(Node previousNode, dynamic data) {
