6c6b4fbde682aefff58252a35e48f0d0a3a6eb47
Albert Dolata <albdolcode@gmail.com>
2023-05-25 12:40:27 +0200

    Create function main to test Stack class

---
+++ data_strucutres/stack_main.dart
@@ -0,0 +1,17 @@
+import 'stack.dart';
+
+void main () {
+  Stack stack = Stack();
+
+  stack.push(43);
+  stack.push(23);
+  // stack.push(14);
+  // stack.push(78);
+  // stack.push(56);
+  // stack.push(12);
+
+  // print(stack.pop());
+  // print(stack.pop());
+  print(stack.pop());
+  print(stack.pop());
+}
