56a3549720ac52f34a57dcad9f6efead0c8fdc90
Albert Dolata <albdolcode@gmail.com>
2023-05-25 12:44:08 +0200

    Stack main function test

--- data_strucutres/stack_main.dart
+++ data_strucutres/stack_main.dart
@@ -5,13 +5,13 @@ void main () {
 
   stack.push(43);
   stack.push(23);
-  // stack.push(14);
-  // stack.push(78);
-  // stack.push(56);
-  // stack.push(12);
+  stack.push(14);
+  stack.push(78);
+  stack.push(56);
+  stack.push(12);
 
-  // print(stack.pop());
-  // print(stack.pop());
+  print(stack.pop());
+  print(stack.pop());
   print(stack.pop());
   print(stack.pop());
 }
