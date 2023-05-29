1513375f8122a2b7f7a97e9db76cf736c9a34259
Albert Dolata <albdolcode@gmail.com>
2023-05-29 23:38:41 +0200

    Create method insert

--- data_strucutres/Tree.dart
+++ data_strucutres/Tree.dart
@@ -2,5 +2,24 @@ import 'TreeNode.dart';
 
 class Tree {
   TreeNode? root;
-  
+
+  void insertData(int data) {
+    if (root == null) {
+      root = TreeNode(data);
+    } else {
+      TreeNode newNode = TreeNode(data);
+      TreeNode? lastNode = root;
+      while (lastNode?.left != null && lastNode?.right != null) {
+        if (lastNode?.left?.left != null && lastNode?.left?.right != null) {
+          lastNode = lastNode?.right;
+        } else {
+          lastNode = lastNode?.left;
+        }
+      } if (lastNode?.left == null) {
+        lastNode?.left = newNode;
+      } else {
+        lastNode?.right = newNode;
+      }
+    }
+  }
 }
