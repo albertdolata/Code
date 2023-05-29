53f341d9a875bb1db413225dab3391f3c88a9d0a
Albert Dolata <albdolcode@gmail.com>
2023-05-29 23:39:19 +0200

    Create main function to test insertData method of class Tree

---
+++ data_strucutres/Tree_main.dart
@@ -0,0 +1,14 @@
+import 'Tree.dart';
+
+void main() {
+  Tree tree = Tree();
+  tree.insertData(1);
+  tree.insertData(2);
+  tree.insertData(3);
+  tree.insertData(4);
+  tree.insertData(5);
+  tree.insertData(6);
+  tree.insertData(7);
+  tree.insertData(8);
+  tree.insertData(9);
+}
