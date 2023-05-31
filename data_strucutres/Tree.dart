081d07ea8e3426c49b3d59150a146c5e823d915e
Albert Dolata <albdolcode@gmail.com>
2023-05-31 12:07:40 +0200

    Another try of implementation of method insertData in calss Tree

--- data_strucutres/Tree.dart
+++ data_strucutres/Tree.dart
@@ -9,13 +9,21 @@ class Tree {
     } else {
       TreeNode newNode = TreeNode(data);
       TreeNode? lastNode = root;
+      TreeNode? previousLastNode;
+      TreeNode? tempNode;
       while (lastNode?.left != null && lastNode?.right != null) {
-        if (lastNode?.left?.left != null && lastNode?.left?.right != null) {
-          lastNode = lastNode?.right;
-        } else {
-          lastNode = lastNode?.left;
-        }
-      } if (lastNode?.left == null) {
+          if (lastNode?.left?.left == null || lastNode?.left?.right == null) {
+            previousLastNode = lastNode;
+            lastNode = lastNode?.left;
+          } else if (previousLastNode?.right?.left == null || previousLastNode?.right?.right == null){
+            tempNode = lastNode;
+            lastNode = previousLastNode?.right;
+            previousLastNode = tempNode;
+          } else {
+            lastNode = previousLastNode?.left;
+          }
+      }
+      if (lastNode?.left == null) {
         lastNode?.left = newNode;
       } else {
         lastNode?.right = newNode;
