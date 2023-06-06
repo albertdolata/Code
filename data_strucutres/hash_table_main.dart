9bda0c4127237267f6f74990c49d7edcf4882352
Albert Dolata <albdolcode@gmail.com>
2023-06-06 23:39:24 +0200

    Create main function to test HashTable class

---
+++ data_strucutres/hash_table_main.dart
@@ -0,0 +1,13 @@
+import 'hash_table.dart';
+
+void main() {
+  HashTable hashTable = HashTable();
+
+  hashTable.insert(1);
+  hashTable.insert(15);
+  hashTable.insert(20);
+  hashTable.insert(14);
+  hashTable.insert(3);
+  hashTable.insert(6);
+  hashTable.insert(1);
+}
