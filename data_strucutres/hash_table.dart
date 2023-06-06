5b9a069ede9839bd248275cbcd0da06535366705
Albert Dolata <albdolcode@gmail.com>
2023-06-07 00:28:26 +0200

    Change type of table List

--- data_strucutres/hash_table.dart
+++ data_strucutres/hash_table.dart
@@ -1,7 +1,7 @@
 import 'hash_data.dart';
 
 class HashTable {
-  List<dynamic> table = List.filled(20, null, growable: false);
+  List<Data?> table = List.filled(20, null, growable: false);
   int index = 0;
 
   int hashIndex(int key) => key % table.length;
@@ -9,7 +9,7 @@ class HashTable {
   void insert (Data data) {
     index = hashIndex(data.key);
     if(table[index] == null){
-      table[index] = data.data;
+      table[index] = data;
     } else {
       while (table[index] != null){
         index++;
@@ -17,7 +17,7 @@ class HashTable {
           index = 0;
         }
       }
-      table[index] = data.data;
+      table[index] = data;
     }
     print("Data inserted at $index index of the list");
   }
