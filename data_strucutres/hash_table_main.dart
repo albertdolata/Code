c6ea93252894c1afc8da44659d612e813ee262da
Albert Dolata <albdolcode@gmail.com>
2023-06-07 00:18:27 +0200

    Changed implementation fo fucntion main to test class HashTable

--- data_strucutres/hash_table_main.dart
+++ data_strucutres/hash_table_main.dart
@@ -1,13 +1,21 @@
+import 'hash_data.dart';
 import 'hash_table.dart';
 
 void main() {
   HashTable hashTable = HashTable();
+  Data pizza = Data('pepperoni', 30);
+  Data age = Data(43, 99);
+  Data car = Data('BMW', 5);
+  Data name = Data('Zbigniew', 998);
+  Data favouriteNumber = Data(7, 50);
+  Data fruit = Data('apple', 20);
+  Data serialNumber = Data (67863, 1);
 
-  hashTable.insert(1);
-  hashTable.insert(15);
-  hashTable.insert(20);
-  hashTable.insert(14);
-  hashTable.insert(3);
-  hashTable.insert(6);
-  hashTable.insert(1);
+  hashTable.insert(pizza);
+  hashTable.insert(age);
+  hashTable.insert(car);
+  hashTable.insert(name);
+  hashTable.insert(favouriteNumber);
+  hashTable.insert(fruit);
+  hashTable.insert(serialNumber);
 }
