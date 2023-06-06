0cf2a595f2e8b719e531f11cb51e5c1cb48e261f
Albert Dolata <albdolcode@gmail.com>
2023-06-07 00:17:51 +0200

    Create hashIndex method in class HashTable and use Data class in HashTable class

--- data_strucutres/hash_table.dart
+++ data_strucutres/hash_table.dart
@@ -1,21 +1,23 @@
 import 'hash_data.dart';
 
 class HashTable {
-  List<int?> table = List.filled(20, null, growable: false);
+  List<dynamic> table = List.filled(20, null, growable: false);
   int index = 0;
 
-  void insert (int data) {
-    index = data % 20;
+  int hashIndex(Data data) => data.key % table.length;
+
+  void insert (Data data) {
+    index = hashIndex(data);
     if(table[index] == null){
-      table[index] = data;
+      table[index] = data.data;
     } else {
       while (table[index] != null){
         index++;
-        if(index > 20) {
+        if(index > table.length) {
           index = 0;
         }
       }
-      table[index] = data;
+      table[index] = data.data;
     }
     print("Data inserted at $index index of the list");
   }
