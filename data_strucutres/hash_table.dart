f713112e92c1c388faeeb0edcc72b38bfa589346
Albert Dolata <albdolcode@gmail.com>
2023-06-06 23:38:52 +0200

    Create insert metod in HashTable class

--- data_strucutres/hash_table.dart
+++ data_strucutres/hash_table.dart
@@ -2,6 +2,20 @@ class HashTable {
   List<int?> table = List.filled(20, null, growable: false);
   int index = 0;
 
-  
+  void insert (int data) {
+    index = data % 20;
+    if(table[index] == null){
+      table[index] = data;
+    } else {
+      while (table[index] != null){
+        index++;
+        if(index > 20) {
+          index = 0;
+        }
+      }
+      table[index] = data;
+    }
+    print("Data inserted at $index index of the list");
+  }
 
 }
