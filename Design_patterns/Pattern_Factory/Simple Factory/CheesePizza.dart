9a8dfeb3b8c35fa0ccbad32a5f62f890694edd5b
Albert Dolata <albdolcode@gmail.com>
2023-06-17 17:36:27 +0200

    Create and implement CheesePizza class

---
+++ Design_patterns/Pattern_Factory/Simple Factory/CheesePizza.dart
@@ -0,0 +1,11 @@
+import 'Pizza.dart';
+
+class CheesePizza extends Pizza {
+  CheesePizza() {
+    name = "Cheese Pizza";
+    dough = "Regular Crust";
+    sauce = "Marinara Pizza Sauce";
+    toppings.add("Fresh Mozzarella");
+    toppings.add("Parmesan");
+  }
+}
