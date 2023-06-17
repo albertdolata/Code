c2ad83a11c15bf4b5c26d369e43d7be5492e6902
Albert Dolata <albdolcode@gmail.com>
2023-06-17 17:11:56 +0200

    Create and implement Pizzeria class

---
+++ Design_patterns/Pattern_Factory/Simple Factory/Pizzeria.dart
@@ -0,0 +1,18 @@
+import 'SimplePizzaFactory.dart';
+
+class Pizzeria {
+  final SimplePizzaFactory factory;
+
+  Pizzeria(this.factory);
+
+  Pizza orderPizza(String type) {
+    Pizza pizza;
+    pizza = factory.createPizza(type);
+
+    pizza.prepare();
+    pizza.bake();
+    pizza.slice();
+    pizza.pack();
+    return pizza;
+  }
+}
