9e9c075b034ec8d45efdd39a9e754fccc1f9b079
Albert Dolata <albdolcode@gmail.com>
2023-06-17 17:00:16 +0200

    Create and implement SimplePizzaFactory class

---
+++ Design_patterns/Pattern_Factory/Simple Factory/SimplePizzaFacotry.dart
@@ -0,0 +1,16 @@
+class SimplePizzaFactory {
+  Pizza createPizza(String type) {
+    Pizza? pizza = null;
+
+    if (type == "cheese") {
+      pizza = CheesePizza();
+    } else if (type == "pepperoni") {
+      pizza = PepperoniPizza();
+    } else if (type == "clam") {
+      pizza = ClamPizza();
+    } else if (type == "veggie") {
+      pizza = VeggiePizza();
+    }
+    return pizza;
+  }
+}
