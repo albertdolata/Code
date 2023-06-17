27e305008e55ecd23f9b653ec809928d24258c30
Albert Dolata <albdolcode@gmail.com>
2023-06-17 17:38:17 +0200

    Create and implement ClamPizza class

---
+++ Design_patterns/Pattern_Factory/Simple Factory/ClamPizza.dart
@@ -0,0 +1,11 @@
+import 'Pizza.dart';
+
+class ClamPizza extends Pizza {
+  ClamPizza() {
+    name = "Clam Pizza";
+    dough = "Thin crust";
+    sauce = "White garlic sauce";
+    toppings.add("Clams");
+    toppings.add("Grated parmesan cheese");
+  }
+}
