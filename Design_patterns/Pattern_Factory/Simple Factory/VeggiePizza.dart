e1e0b51c0ea835eb37a0e5876ea9cba00d41ae34
Albert Dolata <albdolcode@gmail.com>
2023-06-17 17:39:30 +0200

    Create and implement VeggiePizza class

---
+++ Design_patterns/Pattern_Factory/Simple Factory/VeggiePizza.dart
@@ -0,0 +1,15 @@
+import 'Pizza.dart';
+
+class VeggiePizza extends Pizza {
+  VeggiePizza() {
+    name = "Veggie Pizza";
+    dough = "Crust";
+    sauce = "Marinara sauce";
+    toppings.add("Shredded mozzarella");
+    toppings.add("Grated parmesan");
+    toppings.add("Diced onion");
+    toppings.add("Sliced mushrooms");
+    toppings.add("Sliced red pepper");
+    toppings.add("Sliced black olives");
+  }
+}
