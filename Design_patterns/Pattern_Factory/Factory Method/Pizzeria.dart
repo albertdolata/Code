4d7c0595a47c1019cecceef989e6c7ce2f22d4f8
Albert Dolata <albdolcode@gmail.com>
2023-06-18 17:05:35 +0200

    Create and implement Pizzeria class in Factory method folder

---
+++ Design_patterns/Pattern_Factory/Factory Method/Pizzeria.dart
@@ -0,0 +1,15 @@
+abstract class Pizzeria {
+  Pizza orderPizza(String type) {
+    Pizza pizza;
+
+    pizza = createPizza(type);
+
+    pizza.prepare();
+    pizza.bake();
+    pizza.slice();
+    pizza.pack();
+
+    return pizza;
+  }
+  Pizza createPizza(String type);
+}
