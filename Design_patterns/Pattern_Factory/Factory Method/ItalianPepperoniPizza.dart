12873db74d4ca7da2d2529f6c3c253aaa9d87cf6
Albert Dolata <albdolcode@gmail.com>
2023-06-18 17:42:27 +0200

    Create and implement ItalianPepperoniPizza class in Factory method folder

---
+++ Design_patterns/Pattern_Factory/Factory Method/ItalianPepperoniPizza.dart
@@ -0,0 +1,15 @@
+import 'Pizza.dart';
+
+class ItalianPepperoniPizza extends Pizza {
+  ItalianPepperoniPizza() {
+    name = "ItalianClamPizza";
+    dough = "Thin crispy dough";
+    sauce = "Marinara sauce";
+    toppings.add("Grated Reggiano Cheese");
+    toppings.add("Sliced Pepperoni");
+    toppings.add("Garlic");
+    toppings.add("Onion");
+    toppings.add("Mushrooms");
+    toppings.add("Red Pepper");
+  }
+}
