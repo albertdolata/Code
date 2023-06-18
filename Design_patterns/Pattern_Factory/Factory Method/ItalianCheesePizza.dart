70e8a70156753c7279e5869785a1ad6d8a795074
Albert Dolata <albdolcode@gmail.com>
2023-06-18 17:37:03 +0200

    Create and implement ItalianCheesePizza class in Factory method folder

---
+++ Design_patterns/Pattern_Factory/Factory Method/ItalianCheesePizza.dart
@@ -0,0 +1,10 @@
+import 'Pizza.dart';
+
+class ItalianCheesePizza extends Pizza {
+  ItalianCheesePizza() {
+    name = "ItalianCheesePizza with Marinara sauce";
+    dough = "Thin crispy dough";
+    sauce = "Marinara sauce";
+    toppings.add("Grated Reggiano cheese");
+  }
+}
