46b407ad76a2f3523d37be0cc1a2a13f693cc985
Albert Dolata <albdolcode@gmail.com>
2023-06-18 17:49:43 +0200

    Create and implement AmercianPepperoniPizza class in Factory method folder

---
+++ Design_patterns/Pattern_Factory/Factory Method/AmericanPepperoniPizza.dart
@@ -0,0 +1,14 @@
+import 'Pizza.dart';
+
+class AmericanPepperoniPizza extends Pizza {
+  AmericanPepperoniPizza() {
+    name = "American Pepperoni Pizza";
+    dough = "Extra thick, crispy dough";
+    sauce = "Plum Tomato Sauce";
+    toppings.add("Shredded Mozzarella Cheese");
+    toppings.add("Black Olives");
+    toppings.add("Spinach");
+    toppings.add("Eggplant");
+    toppings.add("Sliced Pepperoni");
+  }
+}
