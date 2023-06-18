a93e76026374a1f5cae4994b563a35d5d4d3e5f4
Albert Dolata <albdolcode@gmail.com>
2023-06-18 17:40:34 +0200

    Create and implement ItalianClamPizza class in Factory method folder

---
+++ Design_patterns/Pattern_Factory/Factory Method/ItalianClamPizza.dart
@@ -0,0 +1,11 @@
+import 'Pizza.dart';
+
+class ItalianClamPizza extends Pizza {
+  ItalianCheesePizza() {
+    name = "ItalianClamPizza";
+    dough = "Thin crispy dough";
+    sauce = "Marinara sauce";
+    toppings.add("Grated Reggiano cheese");
+    toppings.add("Fresh Clams from port of Genua");
+  }
+}
