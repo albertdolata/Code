d442e292b5302f9761413a75be02e5ae9e81bb4c
Albert Dolata <albdolcode@gmail.com>
2023-06-18 17:48:51 +0200

    Create and implement AmercianVeggiePizza class in Factory method folder

---
+++ Design_patterns/Pattern_Factory/Factory Method/AmericanVeggiePizza.dart
@@ -0,0 +1,13 @@
+import 'Pizza.dart';
+
+class AmericanVeggiePizza extends Pizza {
+  AmericanVeggiePizza() {
+    name = "American Veggie Pizza";
+    dough = "Extra thick, crispy dough";
+    sauce = "Plum Tomato Sauce";
+    toppings.add("Shredded Mozzarella Cheese");
+    toppings.add("Black Olives");
+    toppings.add("Spinach");
+    toppings.add("Eggplant");
+  }
+}
