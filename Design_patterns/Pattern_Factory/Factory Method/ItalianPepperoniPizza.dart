ca50a46533acde8bfda20c0d1d73d7a02f6bf675
Albert Dolata <albdolcode@gmail.com>
2023-06-18 17:43:35 +0200

    Fix

--- Design_patterns/Pattern_Factory/Factory Method/ItalianPepperoniPizza.dart
+++ Design_patterns/Pattern_Factory/Factory Method/ItalianPepperoniPizza.dart
@@ -2,7 +2,7 @@ import 'Pizza.dart';
 
 class ItalianPepperoniPizza extends Pizza {
   ItalianPepperoniPizza() {
-    name = "ItalianClamPizza";
+    name = "ItalianPepperoniPizza";
     dough = "Thin crispy dough";
     sauce = "Marinara sauce";
     toppings.add("Grated Reggiano Cheese");
