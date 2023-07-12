bc50d18bb967dee201a93d9f8f781797b5cf6daa
Albert Dolata <albdolcode@gmail.com>
2023-07-12 23:18:10 +0200

    Create object of class Singleton before calling method getInstance

--- Design_patterns/pattern_singleton.dart
+++ Design_patterns/pattern_singleton.dart
@@ -1,13 +1,10 @@
 class Singleton {
-  static Singleton? _uniqueInstance;  //static variable stores our instance of class Singleton
+  static Singleton? _uniqueInstance = Singleton();  //static variable stores our instance of class Singleton
   //can add here useful properties
 
   _Singleton() {}  //constructor is private, so only our object of class Singleton can create and use an instance of this class
 
   static Singleton? _getInstance() {    //this method is allowing us to create and return object of class Singleton as a output of a method
-    if (_uniqueInstance == null) {
-      _uniqueInstance == Singleton();
-    }
     return _uniqueInstance;
   }
   //can add here useful methods
